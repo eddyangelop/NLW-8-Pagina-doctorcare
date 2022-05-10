b2df52cb90a45df0f2c69c950fa29f4e9fd28343
Edson Angelo Pereira <87265239+eddyangelop@users.noreply.github.com>
2022-05-10 18:18:42 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,91 @@
+window.addEventListener('scroll', onScroll)
+
+// adiciona um scroll na tela quando for maior que o ponto zero se não remove o scroll no ponto zero //
+onScroll()
+function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  //palavra reservada constante: alvo linha é igual a borda scroll superior mais a  borda inferior dividida por dois
+  const targetLine = scrollY + innerHeight / 2
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+  // o topo a seção
+  const sectionTop = section.offsetTop
+  // a altura da seção
+  const sectionHeight = section.offsetHeight
+
+  // o topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  //informação dos dados e da lógica
+  //console.log('O topo da seção chegou ou passou da linha?',sectionTopReachOrPassedTargetline)
+
+  // verificar se a base está abaixo da linha alvo
+  //quais os dados vou precisar?
+
+  //a seção termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  //o final da seção passou da linha alvo
+  const sectionEndPassedTagetLine = sectionEndsAt <= targetLine
+
+  //console.log('O fundo da seção passou da linha?', sectionEndPassedTagetLine)
+
+  //limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTagetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+/*abre o menu do elemento menu-expanded*/
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+/*retira o menu do elemento menu-expanded*/
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+/*cria interação de revelação com o conteùdo da tela*/
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
